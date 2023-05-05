7b629363531898ae3a003ec155c53690e5aa6070
Akio Ohta <drunkars.p@gmail.com>
2023-05-06 00:41:43 +0900

    Add image copping function.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-
 # Skymagic-Photo-to-2D-model-Tool
+
+## How to start
+
+```
+pip install -r requirements
+streamlit run detect_dots_XYZ_Color_sharpened_20230420.py
+```
--- detect_dots_XYZ_Color_sharpened_20230420.py
+++ detect_dots_XYZ_Color_sharpened_20230420.py
@@ -1,21 +1,51 @@
 import streamlit as st
+from streamlit_cropper import st_cropper
 import cv2
 import numpy as np
 from PIL import Image
 from io import BytesIO
 import base64
 
+def crop_image(image, left, top, right, bottom):
+    return image.crop((left, top, right, bottom))
+
 max_contours = 10
 # Create application title and file uploader widget.
 st.title("Skymagic Photo to 2D model Tool")
 img_file_buffer = st.file_uploader("Choose a file", type=['jpg', 'jpeg', 'png'])
+realtime_update = st.sidebar.checkbox(label="Update in Real Time", value=True)
+box_color = st.sidebar.color_picker(label="Box Color", value='#0000FF')
+aspect_choice = st.sidebar.radio(label="Aspect Ratio", options=["1:1", "16:9", "4:3", "2:3", "Free"])
+aspect_dict = {
+    "1:1": (1, 1),
+    "16:9": (16, 9),
+    "4:3": (4, 3),
+    "2:3": (2, 3),
+    "Free": None
+}
+aspect_ratio = aspect_dict[aspect_choice]
 
 if img_file_buffer is not None:
+
+    img = Image.open(img_file_buffer)
+    if not realtime_update:
+        st.write("Double click to save crop")
+    # Get a cropped image from the frontend
+    cropped_image = st_cropper(img, realtime_update=realtime_update, box_color=box_color,
+                                aspect_ratio=aspect_ratio)
+
+    st.image(cropped_image, caption="Cropped Image", use_column_width=True)
+
+    # convert cropped image to bytearray
+    img_byte_arr = BytesIO()
+    cropped_image.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+
     # Create a value input to get the max_dots from the inputs
     max_contours = st.number_input('Please input the drone number', 1, 30000)
 
     # Read the file and convert it to opencv Image.
-    raw_bytes = np.asarray(bytearray(img_file_buffer.read()), dtype=np.uint8)
+    raw_bytes = np.asarray(bytearray(img_byte_arr), dtype=np.uint8)
     # Loads image in a BGR channel order.
     img = cv2.imdecode(raw_bytes, cv2.IMREAD_COLOR)
     img_out = img.copy()
